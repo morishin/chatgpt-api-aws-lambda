ca99eb8476b86aeed4f2754129d2e297cea7d3ab
morishin <sntr92@gmail.com>
2023-03-09 15:57:59 +0900

    Update

--- app/app.py
+++ app/app.py
@@ -5,12 +5,17 @@ def handler(event, context):
     input = event.get("input")
     if not input:
         return ""
+    gpt_context = os.getenv("GPT_CONTEXT", default="")
+    print("GPT_CONTEXT", gpt_context)
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": os.getenv("GPT_CONTEXT", default="")},
+            {"role": "system", "content": gpt_context},
             {"role": "user", "content": input},
         ]
     )
     message = response["choices"][0]["message"]["content"]
     return message
+
+if __name__ == "__main__":
+    print(handler({"input": "Hello"}, None))
