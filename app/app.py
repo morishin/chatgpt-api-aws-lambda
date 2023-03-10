c04446474974007d836d757702e14a639e001276
morishin <sntr92@gmail.com>
2023-03-10 11:21:11 +0900

    Use type hints

--- app/app.py
+++ app/app.py
@@ -1,25 +1,46 @@
 import os
 import sys
 import openai
+import json
+from typing import TypedDict
 
-def handler(event, context):
-    input = event.get("input")
-    if not input:
-        return ""
-    gpt_context = os.getenv("GPT_CONTEXT", default="")
-    print("GPT_CONTEXT", gpt_context)
+from aws_lambda_typing import context as context_, events
+
+class GPTMessage(TypedDict):
+    role: str
+    content: str
+
+class EventBodyRequired(TypedDict):
+    message: str
+
+class EventBody(EventBodyRequired, total=False):
+    messageHistory: list[GPTMessage]
+
+def handler(event: events.APIGatewayProxyEventV2, context: context_.Context):
+    body: EventBody = json.loads(event["body"])
+
+    initial_system_message = os.getenv("INITIAL_SYSTEM_MESSAGE")
+    print("INITIAL_SYSTEM_MESSAGE: ", initial_system_message)
+
+    messages: list[GPTMessage] = []
+
+    if initial_system_message:
+        messages = [{"role": "system", "content": initial_system_message}]
+
+    if "messageHistory" in body:
+        messages.extend(body["messageHistory"])
+
+    messages.append({"role": "user", "content": body["message"]})
+
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": gpt_context},
-            {"role": "user", "content": input},
-        ]
+        messages=messages
     )
     message = response["choices"][0]["message"]["content"]
     return message
 
 if __name__ == "__main__":
-    if sys.argv.length < 1:
+    if len(sys.argv) < 1:
         print("Usage: python app.py <input>")
         sys.exit(1)
-    print(handler({"input": sys.argv[1]}, None))
+    print(handler({ "body": f'{{ "message": "{sys.argv[1]}" }}' }, None))
